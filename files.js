8ba2a5030221edc4583d5e9a9e4317c181195d30
Matt Vears <matt.vears@gmail.com>
2017-04-09 20:07:21 -0700

    preloading

--- files.js
+++ files.js
@@ -4,57 +4,10 @@
         var path = require("path");
         var imageSize = require("image-size");
         var fileSystem = require("fs");
-        var imageTypes = ["png", "gif", "jpg"];
-        var animated = require('animated-gif-detector');
+        var fileTypes = require("./fileType");
+        var animated = require("animated-gif-detector");
 
-        function scaleRatio(img, screenDimensions) {
-            var imgMax = 0;
-            var dimMax = 0;
-            if (img.dimensions.width > img.dimensions.height) {
-                // picture is wide
-                imgMax = img.dimensions.width;
-                dimMax = screenDimensions.width;
-            } else {
-                // picture is tall
-                imgMax = img.dimensions.height;
-                dimMax = screenDimensions.height;
-            }
-
-            var ratio;
-            if (imgMax > dimMax) { // scale down
-                ratio = imgMax / dimMax;
-                winston.info("scaling image down (ratio: " + ratio + ")");
-                return {
-                    width: img.dimensions.width / ratio,
-                    height: img.dimensions.height / ratio
-                };
-            } else { // scale up
-                ratio = dimMax / imgMax;
-                winston.info("scaling image up (ratio: " + ratio + ")");
-                return {
-                    width: img.dimensions.width * ratio,
-                    height: img.dimensions.height * ratio
-                };
-            }
-        }
-
-        function getImageScalingInfo(img, screenDimensions) {
-            if (img.dimensions.width !== img.dimensions.height) {
-                return scaleRatio(img, screenDimensions);
-            } else { // square.
-                if (screenDimensions.width > screenDimensions.height) {
-                    return {
-                        width: screenDimensions.height,
-                        height: screenDimensions.height
-                    };
-                } else {
-                    return {
-                        width: screenDimensions.width,
-                        height: screenDimensions.width
-                    };
-                }
-            }
-        }
+        
 
         function createFileEntry(name, dir) {
             var joinedPath = path.join(dir, name);
@@ -64,6 +17,7 @@
                 name: name,
                 fullPath: joinedPath,
                 dimensions: dims,
+                handler: fileTypes.getHandler(dims.type),
                 displayLength: function () {
                     if (dims.type === "gif") {
                         if (animated(fileSystem.readFileSync(this.fullPath))) {
@@ -76,32 +30,6 @@
                     } else {
                         throw "image time for " + dims.type;
                     }
-                },
-                render: function(req, res, browserDimensions, callback) {
-                    var dims = browserDimensions;
-                    var that = this;
-                    fileSystem.readFile(this.fullPath,
-                        function (e, d) {
-                            if (e) {
-                                winston.error(e);
-                                process.exit(-1);
-                            }
-
-                            var scaling = getImageScalingInfo(that, browserDimensions);
-
-                            res.write(
-                                "<img src='data:image/" +
-                                dims.type +
-                                ";base64," +
-                                d.toString("base64") +
-                                "' width='" +
-                                scaling.width +
-                                "' height='" +
-                                scaling.height +
-                                "' />");
-
-                            callback(req, res);
-                        });
                 }
             };
         };
@@ -121,8 +49,8 @@
 
                         async.each(items,
                             function(item, cb) {
-                                for (var j = 0; j < imageTypes.length; j++) {
-                                    if (item.indexOf(imageTypes[j]) !== -1) {
+                                for (var j = 0; j < fileTypes.extensions.length; j++) {
+                                    if (item.indexOf(fileTypes.extensions[j]) !== -1) {
                                         tmpFiles.push(createFileEntry(item, imageFolderPath));
                                     }
                                 }
--- server.js
+++ server.js
@@ -19,82 +19,80 @@
 
     files.updateFileList(imageFolderPath);
 
-    fileSystem.readFile("splash-screen.html", function (splashScreenError, splashScreenData) {
-        if (splashScreenError) {
-            winston.error(splashScreenError);
-            process.exit(-2);
-        }
+    fileSystem.readFile("splash-screen.html",
+        function(splashScreenError, splashScreenData) {
+            if (splashScreenError) {
+                winston.error(splashScreenError);
+                process.exit(-2);
+            }
 
-        fileSystem.readFile("picture-frame.html",
-            function(pictureFrameError, pictureFrameData) {
-                if (pictureFrameError) {
-                    winston.error(pictureFrameError);
-                    process.exit(-2);
-                }
+            fileSystem.readFile("picture-frame.html",
+                function(pictureFrameError, pictureFrameData) {
+                    if (pictureFrameError) {
+                        winston.error(pictureFrameError);
+                        process.exit(-2);
+                    }
 
-                // start the server
-                var app = express();
-                app.use(express.static("public"));
-                app.get("/refresh",
-                    function (req, res) {
-                        var refreshTime = 1;
-                        if (req.query["splash"] !== "true") {
-                            var image = req.query["image"];
-                            var imageIndex = parseInt(image, 10);
-                            if (imageIndex === NaN || imageIndex === null || imageIndex === undefined) {
-                                winston.error("invalid image index: " + imageIndex);
-                            } else {
-                                refreshTime = files.files[imageIndex].displayLength();
+                    // start the server
+                    var app = express();
+                    app.use(express.static("public"));
+                    app.get("/refresh",
+                        function(req, res) {
+                            var refreshTime = 1;
+                            if (req.query["splash"] !== "true") {
+                                var image = req.query["image"];
+                                var imageIndex = parseInt(image, 10);
+                                if (imageIndex === NaN || imageIndex === null || imageIndex === undefined) {
+                                    winston.error("invalid image index: " + imageIndex);
+                                } else {
+                                    refreshTime = files.files[imageIndex].displayLength();
+                                }
                             }
-                        }
 
-                        res.writeHead(200, { 'Content-Type': "text/javscript" });
-                        res.write("setTimeout(() => { window.location = window.location; }, " + refreshTime + ");");
-                        res.end();
-                    });
-                app.get("/",
-                    function(req, res) {
-                        // pre processing                
-                        var sessionId = sessions.getOrCreateSessionId(req, res);
-                        if (sessions.get(sessionId) === undefined) {
-                            sessions.create(sessionId);
-                            res.writeHead(200, { 'Content-Type': "text/html" });
-                            res.write(splashScreenData.toString());
+                            res.writeHead(200, { 'Content-Type': "text/javscript" });
+                            res.write("setTimeout(() => { window.location = window.location; }, " + refreshTime + ");");
                             res.end();
-                            return;
-                        }
+                        });
+                    app.get("/",
+                        function(req, res) {
+                            // pre processing                
+                            var sessionId = sessions.getOrCreateSessionId(req, res);
+                            if (sessions.get(sessionId) === undefined) {
+                                sessions.create(sessionId);
+                                res.writeHead(200, { 'Content-Type': "text/html" });
+                                res.write(splashScreenData.toString());
+                                res.end();
+                                return;
+                            }
 
-                        var session = sessions.get(sessionId);
-                        var browserDimensions = sessions.screenDimensions(req, res);
+                            var session = sessions.get(sessionId);
+                            var browserDimensions = sessions.screenDimensions(req, res);
+                            var file = files.files[session.imageIndex];
 
-                        // write content
-                        res.writeHead(200, { 'Content-Type': "text/html" });
-                        var html = pictureFrameData.toString();
-                        html = html.split('IMAGE_ID').join(session.imageIndex);
-                        var htmlParts = html.split("<!-- split -->");
-                        res.write(htmlParts[0]);
-                        files.files[session.imageIndex].render(req,
-                            res,
-                            browserDimensions,
-                            function() {
+                            file.handler.load(file, browserDimensions, function(content) {
+                                res.writeHead(200, { 'Content-Type': "text/html" });
+                                var html = pictureFrameData.toString();
+                                html = html.split("IMAGE_ID").join(session.imageIndex);
+                                var htmlParts = html.split("<!-- split -->");
+                                res.write(htmlParts[0]);
+                                res.write(content);
                                 res.write(htmlParts[1]);
                                 res.end();
                             });
 
-                        // post processing
-                        sessions.get(sessionId).imageIndex += 1;
-                        if (sessions.get(sessionId).imageIndex >= files.files.length) {
-                            sessions.get(sessionId).imageIndex = 0;
-                            files.updateFileList(imageFolderPath);
-                        }
+                            // post processing
+                            sessions.get(sessionId).imageIndex += 1;
+                            if (sessions.get(sessionId).imageIndex >= files.files.length) {
+                                sessions.get(sessionId).imageIndex = 0;
+                                files.updateFileList(imageFolderPath);
+                            }
+                        });
 
-                    });
+                    app.listen(1337,
+                        function() {
+                            winston.info("server started.");
+                        });
 
-                app.listen(1337,
-                    function() {
-                        winston.info("server started.");
-                    });
-
-            });
-    });
+                });
+        });
 }());
